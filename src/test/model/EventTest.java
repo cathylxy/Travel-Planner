efa642252a6588722e6cb252a27336a5c31d8246
xuanya <xuanya@students.cs.ubc.ca>
2022-03-31 22:23:07 -0700

    Add Event test

---
+++ src/test/model/EventTest.java
@@ -0,0 +1,31 @@
+package model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit tests for the Event class
+ */
+public class EventTest {
+    private Event e;
+
+    //NOTE: these tests might fail if time at which line (2) below is executed
+    //is different from time that line (1) is executed.  Lines (1) and (2) must
+    //run in same millisecond for this test to make sense and pass.
+
+    @BeforeEach
+    public void runBefore() {
+        e = new Event("Activity");   // (1)
+    }
+
+    @Test
+    public void testEvent() {
+        assertEquals("Activity", e.getDescription());
+    }
+}
+
